ebfb6e143f99fe1340ac84fbc754b132fd7fd371
kkwteh <kkwteh@squareup.com>
2013-07-21 13:55:25 -0700

    This script converts the date column to a more useful time series index for the data frame

---
+++ set_index_time_series.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('./data/train.csv')
+def year(n):
+    return str(n /10000)
+def month(n):
+    return str(20121122 % 10000 / 100)
+def day(n):
+    return str(20121122 % 100)
+def datestring(n):
+    return year(n) + '-' + month(n) + '-' + day(n)
+df.index = pd.to_datetime([datestring(n) for n in df.Date])
