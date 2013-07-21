c5267d64a16aab1ead50e19dd88461c85bb3cdb0
kkwteh <kkwteh@gmail.com>
2013-07-21 14:18:34 -0700

    Refactor year, month, day helpers

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 df = pd.read_csv('./data/train.csv')
-def year(n):
-    return str(n /10000)
-def month(n):
-    return str(n % 10000 / 100)
-def day(n):
-    return str(n % 100)
+def _year(n):
+    return str(n)[:4]
+def _month(n):
+    return str(n)[4:6]
+def _day(n):
+    return str(n)[6:]
 def datestring(n):
-    return year(n) + '-' + month(n) + '-' + day(n)
-df.index = pd.to_datetime([datestring(n) for n in df.Date])
+    return _year(n) + '-' + _month(n) + '-' + _day(n)
+
+
+df.index = pd.to_datetime([datestring(n) for n in date_list])
