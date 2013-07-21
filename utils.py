a92d532280d17e98e36b8e367ee870e3f8214856
kkwteh <kkwteh@gmail.com>
2013-07-21 14:23:05 -0700

    Hide datestring helper

--- utils.py
+++ utils.py
@@ -9,9 +9,9 @@ def _month(n):
 def _day(n):
     return str(n)[6:]
     
-def datestring(n):
+def _datestring(n):
     return _year(n) + '-' + _month(n) + '-' + _day(n)
 
 #example usage: df.index = datestring_index(df.Date)
 def datestring_index(date_int_list):
-    pd.to_datetime([datestring(n) for n in date_int_list])
+    pd.to_datetime([_datestring(n) for n in date_int_list])
