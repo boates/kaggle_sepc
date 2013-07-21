c5c5ff843a5e993acd226cf431ad51d623caf340
George Lewis <george@lendup.com>
2013-07-21 14:41:59 -0700

    Changed naming scheme for global station and probe objects

--- utils.py
+++ utils.py
@@ -4,8 +4,10 @@ import csv
 
 from station import Station
 
+# Global variables (for now)
+STATIONS = {}
+PROBES = {}
 
-STATION_INFO = {}
 
 def _year(n):
     return str(n)[:4]
@@ -23,26 +25,26 @@ def datestring_index(date_int_list):
     return pd.to_datetime([_datestring(n) for n in date_int_list])
 
 def populate_stations():
-    # load the csv
-
     station_file  = open('data/station_info.csv', "rb")
     station_data = csv.reader(station_file)
-    #station_data = load_csv("data/station_info.csv")
-
     for name, lat, lon, elev in station_data:
         station = Station(name, lat, long, elev)
-        STATION_INFO[name] = station
+        STATIONS[name] = station
+
 
 def get_station(station_name):
-    return STATION_INFO[station_name]
+    return STATIONS[station_name]
+
 
 def get_features_from_station(station_name):
     return []
+
 
 def get_features_at_location(lat, lon, elivation):
     station = get_closest_station(lat, lon, elivation)
     features = get_features_from_station(station)
     return features
+
 
 def main():
     populate_stations()
