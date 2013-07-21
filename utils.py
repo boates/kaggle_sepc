efd89225703550427292147396fdf3a6a546aa27
kkwteh <kkwteh@gmail.com>
2013-07-21 14:20:30 -0700

    Put datestring_index helper into useable form

--- utils.py
+++ utils.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv('./data/train.csv')
 def _year(n):
     return str(n)[:4]
+    
 def _month(n):
     return str(n)[4:6]
+    
 def _day(n):
     return str(n)[6:]
+    
 def datestring(n):
     return _year(n) + '-' + _month(n) + '-' + _day(n)
 
-
-df.index = pd.to_datetime([datestring(n) for n in date_list])
+#example usage: df.index = datestring_index(df.Date)
+def datestring_index(date_int_list):
+    pd.to_datetime([datestring(n) for n in date_int_list])
