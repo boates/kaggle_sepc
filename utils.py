3dbbd3c5ec740ed3636d5c92c01de11b1694ab33
Diane Wu <dianew@palantir.com>
2013-07-21 15:55:12 -0700

    add getDistance option to find_closest_distances

--- utils.py
+++ utils.py
@@ -90,11 +90,16 @@ def train_model(features):
     print "DUMMY 'train_model'"
     return None
 
-def find_closest_coordinates(coord1, coord2_list, n=1, distanceMethod = distance.GreatCircleDistance):
+def find_closest_coordinates(coord1, coord2_list, n=1, distanceMethod = distance.GreatCircleDistance, getDistances = False):
     distance.distance = distanceMethod  
     distances = [distance.distance(Point(coord1['lat'], coord1['lon'],coord1['elev']), Point(coord2['lat'], coord2['lon'],coord2['elev'])).miles for idx, coord2 in coord2_list.iterrows()]
     maxd = np.max(heapq.nsmallest(n, distances))
-    return coord2_list[(distances<=maxd)]
+    if getDistances:
+        df_ret = copy.deepcopy(coord2_list)
+        df_ret['dist'] = distances
+        return df_ret[df_ret['distances']<=maxd]
+    else:
+        return coord2_list[(distances<=maxd)]
 
 def classify(model, features):
     print "DUMMY 'classify'"
