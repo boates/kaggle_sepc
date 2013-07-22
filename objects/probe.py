a271de4ab91f73518e60277f0c21051eb9d9fc09
George Lewis <george@lendup.com>
2013-07-21 19:03:40 -0700

    Fixing probe methods

--- objects/probe.py
+++ objects/probe.py
@@ -12,18 +12,18 @@ class Probe(object):
         self.features = None
     
     def __str__(self):
-        s  = '(i, j): ('+self.i+','+self.j+')'+'\n'
-        s += 'latitude: '+self.lat+'\n'
-        s += 'longitude: '+self.lon+'\n'
+        s  = '(i, j): (%s,%s) \n' % (self.i, self.j)
+        s += 'latitude: %s\n' % self.lat
+        s += 'longitude: %s\n' % self.lon
         return s
     
     def __repr__(self):
         return self.__str__()
     
-    def get_lat(lat_0=31):
+    def get_lat(self, lat_0=31):
         return lat_0 + self.i
     
-    def get_lon(lon_0=106):
+    def get_lon(self, lon_0=106):
         return lon_0 - self.j
     
 
